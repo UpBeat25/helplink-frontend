e530253336828c9f2a6ce288c250e01df3ffe0dd
siddh <113300714+UpBeat25@users.noreply.github.com>
2025-11-25 14:00:43 +0530

    Different adapter deploy 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
-  kit: {
-    adapter: adapter(),
-  }
+export default {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter()
+    }
 };
-
-export default config;
